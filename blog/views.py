6255591000ce3269a8e285089fb381cbaf60bf10
jungsuji <wtw9611@gmail.com>
2019-12-11 17:16:14 +0900

    post_list.html에서 동적으로 Post QuerySet을 순회하며 데이터 출력

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 import os
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from blog.models import Post
 
 def post_list(request):
     # Template를 찾을 경로에서 post_list.html을 찾아서
     # 그 파일을 text로 만들어서 HttpResponse 형태로 돌려준다.
     # 위 기능을 하는 shortcut 함수
-    return render(request, 'post_list.html')
+
+    # 1. posts라는 변수에 전체 Post를 가지는 QuerySet 객체를 할당
+    #   hint) Post.objects.무언가... 를 실행한 결과는 QuerySet 객체가 된다.
+    # 2. context라는 dict를 생성하며, 'post'키에 위에 posts변수를 value로 사용하도록 한다.
+    # 3. render의 3번째 위치인자로 위 context 변수를 전달한다.
+
+    posts = Post.objects.all()
+    context = {'posts': posts}
+
+    return render(request, 'post_list.html', context)
